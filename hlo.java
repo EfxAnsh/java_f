5bfcdd318b4bc6a5fd2ae57e6cdd5233fd8a814b
EfxAnsh <anshumanrangarh1@gmail.com>
2025-01-07 20:15:55 +0530

    hey

--- hlo.java
+++ hlo.java
@@ -1,5 +1,7 @@
-public class hlo {
+public class hlo{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("My name is Anshuman Rangarh.");
+        System.out.println("I am in B.Tech Computer Science Engineering (CSE).");
     }
 }
