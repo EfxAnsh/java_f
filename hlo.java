058307b53d1df47179e758ad5176ad6fd0bcdd40
EfxAnsh <anshumanrangarh1@gmail.com>
2025-02-04 04:19:21 +0530

    deletion

--- hlo.java
+++
@@ -1,7 +0,0 @@
-public class hlo{
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("My name is Anshuman Rangarh.");
-        System.out.println("I am in B.Tech Computer Science Engineering (CSE).");
-    }
-}
